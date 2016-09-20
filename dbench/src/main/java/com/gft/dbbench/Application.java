a12a47a8c997691da834d6d4fa19d0847531b123
michalsima <michal.sima.dev@gmail.com>
2016-09-20 11:01:05 +0200

    Oracle and H2 datasources

---
+++ dbench/src/main/java/com/gft/dbbench/Application.java
@@ -0,0 +1,14 @@
+package com.gft.dbbench;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	}
+
+}
